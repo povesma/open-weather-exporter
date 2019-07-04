2b5e62d44fc079fd5d67c10f8eec52119e357ebb
Andy Stoica <andy.stoica@me.com>
2019-07-04 06:57:27 +0100

    Added OpenMetric and JSON outputs

--- app.js
+++ app.js
@@ -1,48 +1,130 @@
 'use strict';
-
 // TODOs
 // =====
-// Done. Setup routes and 404 error handlers
-// Use ENV variables for configuration
-// Function for fetching data from OpenWeatehrMap.org
-// Convert data to Open Metrics
-// Convert data to custom JSON format
-// Export data over HTTP
 
+require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 
-const PORT = 8080;
-const HOST = '0.0.0.0';
+const PORT = process.env.OPEN_WEATHER_EXPORTER_PORT || '8000';
+const HOST = process.env.OPEN_WEATHER_EXPORTER_HOST || '0.0.0.0';
+const API_KEY = process.env.OPEN_WEATHER_EXPORTER_API_KEY || 'api_key_required';
+const API_ENDPOINT = `https://api.openweathermap.org/data/2.5/weather`;
+const API_LOCATION_ID = process.env.OPEN_WEATHER_EXPORTER_LOCATION_ID || '26136439';
+const API_STRING = `${API_ENDPOINT}?id=${API_LOCATION_ID}&units=metric&appid=${API_KEY}`;
 
 const app = express();
+
+
+// Fetches data from OpenWeather API
+async function fetchWeatherData() {
+  const response = await axios.get(API_STRING);
+  return response.data;
+}
+
+
+// Formats a single sensor reading
+function formatOpenMetricsSensor(sensor) {
+  let output = '';
+  output += `#HELP ${sensor.name} ${sensor.description}\n`;
+  output += `#TYPE ${sensor.name} ${sensor.type}\n`;
+  output += `${sensor.name}{location="${sensor.source}"} ${sensor.value}\n`;
+  return output;
+};
+
+
+// Formats OpenWeather data to Open Metrics for Prometheus
+function formatOpenMetrics(data) {
+  let output = '';
+  let sensor = {};
+  
+  // Air Tempearture
+  sensor = {
+    name: 'sensor_air_temperature',
+    description: 'Air temperature in degrees Celsius (˚C)',
+    type: 'guage',
+    source: 'OpenWeather',
+    value: data.main.temp
+  };
+  output += formatOpenMetricsSensor(sensor);
+  
+  // Air Relative humidity
+  sensor = {
+    name: 'sensor_air_relative_humidity',
+    description: 'Air relative humidity in percentage (%H)',
+    type: 'guage',
+    source: 'OpenWeather',
+    value: data.main.humidity
+  };
+  output += formatOpenMetricsSensor(sensor);
+  
+  // Air Pressure
+  sensor = {
+    name: 'sensor_air_pressure',
+    description: 'Air pressure in hectopascals (hPa)',
+    type: 'guage',
+    source: 'OpenWeather',
+    value: data.main.pressure
+  };
+  output += formatOpenMetricsSensor(sensor);
+  
+  return output;
+}
+
+
+// Converts readings from OpenWeather API to a sipler JSON Output
+function formatJsonMetrics(data) {
+  const sensorData = {
+    air_temperature: data.main.temp,
+    air_relative_humidity: data.main.humidity,
+    air_pressure: data.main.pressure
+  };
+  return sensorData;
+}
+
 
 // Handles requests to home page
 app.get('/', (req, res, next) => {
   res.send('OpenWeatherMap.org exporter for Prometheus');
 });
 
+
 // Returns Open Metrics formatted data for Prometheus
-app.get('/metrics', (req, res, next) => {
-  res.send('Metrics');
+app.get('/metrics', async (req, res, next) => {
+  try {
+    const data = await fetchWeatherData();
+    const metrics = formatOpenMetrics(data)
+    res.status(200).send(metrics);
+  } catch (err) {
+    return next(err);
+  }
 });
 
+
 // Returns JSON formated data
-app.get('/sensors', (req, res, next) => {
-  return next(new Error('Could not fetch data from OpenWeatherMap.org'));
-  res.send('Sensors');
-});
+app.get('/sensors', async (req, res, next) => {
+  try {
+    const data = await fetchWeatherData();
+    const metrics = formatJsonMetrics(data)
+    res.status(200).send(metrics);
+  } catch (err) {
+    return next(err);
+}});
+
 
 // Handles 404 errors
 app.use((req, res, next) => {
-  res.status(404).send('Resource not found');
+  res.status(404).send('404 Not found');
 })
+
 
 // Handles thrown errors
 app.use((err, req, res, next) => {
-  res.status(500).send(err.message);
+  console.error(err.stack);
+  res.status(500).send('500 Server error');
 })
 
 
+// Start listening for requests
 app.listen(PORT, HOST);
 console.log(`OpenWeather Exporter listening on http://${HOST}:${PORT}`);
